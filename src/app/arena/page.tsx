0af7d17788737e540c771045532d03a88f161bc9
gulshanpr <gulshankumarprasad04@gmail.com>
2024-09-29 00:29:13 +0530

    init 2

---
+++ src/app/arena/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Arena = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/arena-bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
+     
+    </div>
+  );
+};
+
+export default Arena;
